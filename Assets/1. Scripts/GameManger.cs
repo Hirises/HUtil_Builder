e530dfa16018aa79052c07f7b133910c5918e86f
Hirises <hirises1243@gmail.com>
2026-03-12 12:56:30 +0900

    feat: L10N 테스트

--- Assets/1. Scripts/GameManger.cs
+++ Assets/1. Scripts/GameManger.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 using HUtil.UI;
 using HUtilBuilder;
+using HUtil.Runtime.L10N;
 
 public class GameManger : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class GameManger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        L10NConverter.SetLocale("ko_KR");
         var subUIValue = new SubUIValue();
         BindingContext.StaticBind(subUIValue);
         playerData = new MainUIValue(subUIValue);
